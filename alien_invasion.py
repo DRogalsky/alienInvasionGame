871c6c81a923b599df5120530d8a37d39f7c0250
dnl092 <drogalsky14@my.whitworth.edu>
2020-02-27 23:38:37 -0800

    added the ship object, the ship image, and simplified the run_game method

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys, pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -13,21 +14,31 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         #set background color
 
     def run_game(self):
         """start the main loop for the game"""
         while True:
-            #Watch for keyboard ad mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            #Redraw the screen durin each pass through the loop
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        #checks for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            #make most recently drawn screen visible
-            pygame.display.flip()
+    def _update_screen(self):
+        #Redraw the screen durin each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        #make most recently drawn screen visible
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     #make a game instance, and run the game
