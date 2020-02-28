672f275302c9d917faf3a6dcc02d1fdd7e1b93d6
dnl092 <drogalsky14@my.whitworth.edu>
2020-02-28 00:04:38 -0800

    added ship movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         """start the main loop for the game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -29,6 +30,22 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #move ship to the right
+                    self.ship.moving_right = True
+
+                elif event.key == pygame.K_LEFT:
+                    #move ship to the left
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                #stop movement
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         #Redraw the screen durin each pass through the loop
