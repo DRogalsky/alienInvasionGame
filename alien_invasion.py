862adc0554ef9370d20d12bfb51e406ae6935012
dnl092 <drogalsky14@my.whitworth.edu>
2020-02-29 08:25:26 -0800

    added bullets"

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys, pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -19,13 +20,14 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
-        #set background color
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """start the main loop for the game"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -50,6 +52,8 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
             #press q to quit
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """respond to key releases"""
@@ -60,10 +64,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         #Redraw the screen durin each pass through the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         #make most recently drawn screen visible
         pygame.display.flip()
