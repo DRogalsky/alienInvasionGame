131937efc8bac128b5e133c91b83931a587e241d
dnl092 <drogalsky14@my.whitworth.edu>
2020-03-06 11:33:23 -0800

    added reset code and made the button unclickable after it disapears

--- alien_invasion.py
+++ alien_invasion.py
@@ -89,8 +89,19 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks play"""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            #reset game stats
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            #get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+
+            #create a new fleet and center ship
+            self._create_fleet()
+            self.ship.center_ship()
 
     #Bullet Methods
 
