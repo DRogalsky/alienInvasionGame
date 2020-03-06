58010ca6e8b998cac4b8be2488f4b33afb8a8698
dnl092 <drogalsky14@my.whitworth.edu>
2020-03-05 16:56:12 -0800

    aliens now move right off the screen lol

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -115,6 +116,9 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet"""
+        self.aliens.update()
 
     def _update_screen(self):
         #Redraw the screen durin each pass through the loop
